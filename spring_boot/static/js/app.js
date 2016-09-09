8068649cda435ae72616197ab959bebb7c1cd2a0
Space Invader <ne0@poczta.onet.pl>
2016-09-09 15:20:13 +0200

    run

--- spring_boot/static/js/app.js
+++ spring_boot/static/js/app.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var app = angular.module('productParametersApp', []);
 
 app.factory('ProductRepository', function ($resource) {
@@ -75,3 +77,38 @@ app.factory('ProductRepository', function ($resource) {
 	self.loadProductParametersType();
 	self.loadProductsList();
 });
+
+app.directive('productParamEditorWidget', [function() {
+	return {
+		templateUrl: 'js/productParamEditorWindow.html',
+		restrict: 'E',
+		scope: {
+			parameterTypesData: '=',
+			control: '=',
+			whenSaveParam: '&'
+		},
+		link: function($scope, $element, $attrs) {
+			$scope.productParamData = {};
+			
+			$scope.labels = {
+				parameterValue: 'Wartość:',
+				parameterBigValue: 'Duża wartość:'
+			};
+
+			$scope.internalParamWindowControl = $scope.control || {};
+			$scope.internalParamWindowControl.showWindow = function(param) {
+				$scope.productParamData = param;
+				$('#addParamModal').modal("show");				
+			}; 
+			$scope.internalParamWindowControl.hideWindow = function() {
+				$('#addParamModal').modal("hide");
+			};
+			
+			$scope.saveParam = function() {
+				$scope.whenSaveParam({
+					param: $scope.productParamData
+				});
+			};
+		}
+	}
+}]);
