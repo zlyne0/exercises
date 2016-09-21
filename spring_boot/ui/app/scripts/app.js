8782f27c3fdbf2d7ba7c301a5175249be53822fd
Space Invader <ne0@poczta.onet.pl>
2016-09-21 11:25:24 +0200

    yoman project configuration

---
+++ spring_boot/ui/app/scripts/app.js
@@ -0,0 +1,148 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name appApp
+ * @description
+ * # appApp
+ *
+ * Main module of the application.
+ */
+var app = angular
+  .module('productParametersApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
+
+app.factory('ProductRepository', function ($resource) {
+	return $resource( 'rest/product/:productId/addParam', { productId: '@productId' } );
+});
+
+app.controller('productParametersController', function($scope, $http) {
+	self = this;
+
+	self.setSelectedProduct = function(product) {
+		$scope.selectedParam = null;
+		$scope.selectedProduct = product;
+		self.loadProductParameters();
+	}
+
+	self.setSelectedParam = function(param) {
+		$scope.selectedParam = param;
+	}
+
+	self.loadProductParameters = function() {
+		$http.get("rest/product/" + $scope.selectedProduct.id + "/parameters").then(function(response) {
+			$scope.productParameters = response.data;
+		});
+	}
+
+	self.loadProductsList = function() {
+		$scope.selectedProduct = null;
+		$scope.productParameters = [];
+
+		$http.get("rest/product/list").then(function(response) {
+			$scope.products = response.data;
+		});
+	}
+
+	self.loadProductParametersType = function() {
+		$http.get("rest/parameterType/list").then(function(response) {
+			$scope.parameterTypes = response.data;
+		});
+	}
+
+	$scope.paramWindowControl = {};
+
+	self.onSaveParam = function(param) {
+		console.log('onSaveParam ' + JSON.stringify(param));
+
+		var data = {
+			id: param.id,
+			productId: $scope.selectedProduct.id,
+			type: {
+				id: param.type.id
+			},
+			value: param.value,
+			bigValue: param.bigValue
+		};
+		$http.post("rest/product/" + $scope.selectedProduct.id + "/addParam", data).then(
+			function onSuccess(response) {
+				self.loadProductParameters();
+			},
+			function onError(response) {
+				alert('error status: ' + response.status + ', data: ' + response.data);
+			}
+		);
+
+		$scope.paramWindowControl.hideWindow();
+	}
+
+	self.openParamWindow = function(modalId) {
+		$scope.paramWindowControl.showWindow({});
+	}
+
+	self.openEditParamWindow = function(modalId) {
+		$scope.paramWindowControl.showWindow(
+			JSON.parse(JSON.stringify($scope.selectedParam))
+		);
+	}
+
+	self.loadProductParametersType();
+	self.loadProductsList();
+});
+
+app.directive('productParamEditorWidget', [function() {
+	return {
+		templateUrl: 'scripts/productParamEditorWindow.html',
+		restrict: 'E',
+		scope: {
+			parameterTypesData: '=',
+			control: '=',
+			whenSaveParam: '&'
+		},
+		link: function($scope, $element, $attrs) {
+			$scope.productParamData = {};
+
+			$scope.labels = {
+				parameterValue: 'Wartość:',
+				parameterBigValue: 'Duża wartość:'
+			};
+
+			$scope.internalParamWindowControl = $scope.control || {};
+			$scope.internalParamWindowControl.showWindow = function(param) {
+				$scope.productParamData = param;
+				$('#addParamModal').modal("show");
+			};
+			$scope.internalParamWindowControl.hideWindow = function() {
+				$('#addParamModal').modal("hide");
+			};
+
+			$scope.saveParam = function() {
+				$scope.whenSaveParam({
+					param: $scope.productParamData
+				});
+			};
+		}
+	}
+}]);
---
+++ spring_boot/ui/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name appApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the appApp
+ */
+angular.module('productParametersApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
