bbf75820cab9bb1c0bb45937f81ead7374cb882b
Space Invader <ne0@poczta.onet.pl>
2016-12-07 13:28:22 +0100

    remove old ui

--- spring_boot/src/main/java/promitech/web_angular_exp/services/ProductParameterRepository.java
+++
@@ -1,9 +0,0 @@
-package promitech.web_angular_exp.services;
-
-import org.springframework.data.repository.CrudRepository;
-
-import promitech.web_angular_exp.model.ProductParameter;
-
-public interface ProductParameterRepository extends CrudRepository<ProductParameter, Long> {
-
-}
--- spring_boot/src/main/java/promitech/web_angular_exp/services/ProductParameterTypeRepository.java
+++
@@ -1,9 +0,0 @@
-package promitech.web_angular_exp.services;
-
-import org.springframework.data.repository.CrudRepository;
-
-import promitech.web_angular_exp.model.ProductParameterType;
-
-public interface ProductParameterTypeRepository extends CrudRepository<ProductParameterType, Long> {
-
-}
