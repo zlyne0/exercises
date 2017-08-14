19cad9c8ed2d9cf54d287c8e0d8df1b1ccd65ac8
Space Invader <ne0@poczta.onet.pl>
2017-08-14 12:26:39 +0200

    ag version 1.1

--- ag/static/app.js
+++ ag/static/app.js
@@ -1,23 +1,60 @@
-(function() {'use strict';
+(function() {
+'use strict';
 
-angular.module("anterApp", ["ngRoute"])
-.config(function($routeProvider) {
+angular
+.module("anterApp", ["ngRoute", "ngMaterial"])
+.config(function($routeProvider, $mdThemingProvider) {
+	
     $routeProvider
     .when("/", {
-        templateUrl : "company.html"
+        templateUrl : "aboutUs.html"
     })
     .when("/employer", {
         templateUrl : "employer.html"
     })
+    .when("/forEmployer", {
+    	templateUrl : "forEmployer.html"
+    })
     .when("/employee", {
         templateUrl : "employee.html"
     })
-    .when("/company", {
-        templateUrl : "company.html"
+    .when("/aboutUs", {
+        templateUrl : "aboutUs.html"
     })
     .when("/contact", {
         templateUrl : "contact.html"
-    });
-});	
+    })
+    .when("/workInPolandForForeigners", {
+    	templateUrl : "workInPolandForForeigners.html"
+    })
+    .when("/foreignersService", {
+    	templateUrl : "foreignersService.html"
+    })
+    
+    .when("/workNetherlands", {
+    	templateUrl : "workNetherlands.html"
+    })
+    .when("/workFrance", {
+    	templateUrl : "workFrance.html"
+    })
+    .when("/workGermany", {
+    	templateUrl : "workGermany.html"
+    })
+    .when("/workBelgium", {
+    	templateUrl : "workBelgium.html"
+    })
+    
+})	
+.controller('MenuCtrl', function MenuCtrl($mdDialog, $location) {
+	var originatorEv;
+	this.openMenu = function($mdMenu, ev) {
+		originatorEv = ev;
+		$mdMenu.open(ev);
+	};
+	
+	this.menuButtonClick = function(routeStr) {
+		$location.path(routeStr);
+	};
+});
 
 })();
