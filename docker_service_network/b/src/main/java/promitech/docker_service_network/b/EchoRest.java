d72d67f3a33b3f4c5240374d4e00edcffeffcee1
Space Invader <ne0@poczta.onet.pl>
2018-04-17 16:44:45 +0200

    docker_service_network

---
+++ docker_service_network/b/src/main/java/promitech/docker_service_network/b/EchoRest.java
@@ -0,0 +1,20 @@
+package promitech.docker_service_network.b;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/rest")
+public class EchoRest {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(EchoRest.class);
+    
+    @RequestMapping("/echo/{str}")
+    public String echo(@PathVariable("str") String str) {
+        return "b" + str + "b";
+    }
+    
+}
---
+++ docker_service_network/c/src/main/java/promitech/docker_service_network/c/Main.java
@@ -0,0 +1,28 @@
+package promitech.docker_service_network.c;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
+
+@SpringBootApplication
+@ComponentScan(basePackages = {"promitech.docker_service_network"})
+@EnableDiscoveryClient
+public class Main {
+
+    @Bean
+    public CommonsRequestLoggingFilter requestLoggingFilter() {
+        CommonsRequestLoggingFilter crlf = new CommonsRequestLoggingFilter();
+        crlf.setIncludeClientInfo(true);
+        crlf.setIncludeQueryString(true);
+        crlf.setIncludePayload(true);
+        return crlf;
+    }    
+     
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Main.class, args);
+    }
+    
+}
