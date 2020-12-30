5aeb4085cd13f2300ce09d50059501e4d6ef58ab
Space Invader <ne0@poczta.onet.pl>
2020-12-30 18:29:48 +0100

    kube_service_network kube configuration

--- kube_service_network/a/src/main/java/promitech/docker_service_network/a/Main.java
+++ kube_service_network/a/src/main/java/promitech/docker_service_network/a/Main.java
@@ -2,14 +2,11 @@ package promitech.docker_service_network.a;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.filter.CommonsRequestLoggingFilter;
 
 @SpringBootApplication
-//@ComponentScan(basePackages = {"promitech.docker_service_network"})
 @EnableFeignClients
 public class Main {
 
