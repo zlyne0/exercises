52f53137a16823b10cb68179897b5e815113726e
Space Invader <ne0@poczta.onet.pl>
2023-11-24 10:31:08 +0100

    pesel

---
+++ enterprise_common_components/src/main/kotlin/promitech/ecc/Pesel.java
@@ -0,0 +1,86 @@
+package promitech.ecc;
+
+import java.time.LocalDate;
+
+public class Pesel {
+
+    private final String value;
+
+    Pesel(String value) {
+        this.value = value;
+    }
+
+    public boolean isValid() {
+        return Validator.validate(value) == ValidationResult.VALID;
+    }
+
+    public Sex extractSex() {
+        if (!isValid()) {
+            throw new IllegalStateException("pesel '" + value + "' is invalid");
+        }
+        if (Integer.parseInt(String.valueOf(value.charAt(9))) % 2 == 0) {
+            return Sex.FEMALE;
+        } else {
+            return Sex.MALE;
+        }
+    }
+
+    public LocalDate extractBirthDate() {
+        if (!isValid()) {
+            throw new IllegalStateException("pesel '" + value + "' is invalid");
+        }
+        int year = Integer.parseInt(value.substring(0, 2));
+        int monthCode = Integer.parseInt(value.substring(2, 4));
+        int day = Integer.parseInt(value.substring(4, 6));
+        int century = 1900;
+        int month = monthCode;
+        if (monthCode > 80) {
+            century = 1800;
+            month = monthCode - 80;
+        } else if (monthCode > 60) {
+            century = 2200;
+            month = monthCode - 60;
+        } else if (monthCode > 40) {
+            century = 2100;
+            month = monthCode - 40;
+        } else if (monthCode > 20) {
+            century = 2000;
+            month = monthCode - 20;
+        }
+        return LocalDate.of(century + year, month, day);
+    }
+
+    public enum Sex {
+        FEMALE, MALE
+    }
+
+    public enum ValidationResult {
+        VALID, INVALID
+    }
+
+    public static class Validator {
+
+        public static final int PESEL_LENGTH = 11;
+        public static int[] WEIGHTS = {1, 3, 7, 9, 1, 3, 7, 9, 1, 3};
+
+        public static ValidationResult validate(String pesel) {
+            if (pesel.length() != PESEL_LENGTH) {
+                return ValidationResult.INVALID;
+            }
+            if (!pesel.matches("\\d+")) {
+                return ValidationResult.INVALID;
+            }
+            int sum = 0;
+            for (int i = 0; i < pesel.length()-1; i++) {
+                sum += WEIGHTS[i] * Integer.parseInt(String.valueOf(pesel.charAt(i)));
+            }
+            int control = 10 - (sum % 10);
+            int lastDigit = Integer.parseInt(String.valueOf(pesel.charAt(10)));
+            if (lastDigit != control) {
+                return ValidationResult.INVALID;
+            }
+            return ValidationResult.VALID;
+        }
+    }
+
+}
---
+++ enterprise_common_components/src/test/kotlin/promitech/ecc/PeselTest.java
@@ -0,0 +1,53 @@
+package promitech.ecc;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.time.LocalDate;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PeselTest {
+
+    @ParameterizedTest
+    @CsvSource({
+            "1, INVALID",
+            "1234567890a, INVALID",
+            "98040477747, INVALID",
+            "05252442983, VALID",
+            "98040477748, VALID"
+    })
+    void should_validate_pesel(String pesel, Pesel.ValidationResult result) {
+        assertThat(Pesel.Validator.validate(pesel)).isEqualTo(result);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "80021576555, MALE",
+            "01282019537, MALE",
+            "01301846559, MALE",
+            "55090441277, MALE",
+            "64080477952, MALE",
+            "54010385882, FEMALE",
+            "88022977245, FEMALE",
+            "02292654886, FEMALE",
+            "99092061963, FEMALE",
+            "68072986886, FEMALE"
+    })
+    void should_extract_sex(String pesel, Pesel.Sex sex) {
+        assertThat(new Pesel(pesel).extractSex()).isEqualTo(sex);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "25082194649, 1925-08-21",
+            "78060148149, 1978-06-01",
+            "52052868523, 1952-05-28",
+            "79230381483, 2079-03-03",
+            "39271449261, 2039-07-14",
+            "06241697182, 2006-04-16"
+    })
+    void should_extract_birth_date(String pesel, String dateStr) {
+        assertThat(new Pesel(pesel).extractBirthDate()).isEqualTo(LocalDate.parse(dateStr));
+    }
+}
