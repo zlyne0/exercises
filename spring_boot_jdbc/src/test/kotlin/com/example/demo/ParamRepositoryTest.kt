8c6bdbdf6076bb9d46af56353d9923eb2504bffa
Space Invader <ne0@poczta.onet.pl>
2024-02-04 12:01:31 +0100

    spring_boot_jdbc - liquibase

---
+++ spring_boot_jdbc/src/test/kotlin/com/example/demo/ParamRepositoryTest.kt
@@ -0,0 +1,20 @@
+package com.example.demo
+
+import org.junit.jupiter.api.Test
+import org.springframework.test.context.TestPropertySource
+
+//@TestPropertySource(
+//    properties = ["spring.liquibase.change-log=classpath:db.test.changelog/blob_test.xml"]
+//)
+class ParamRepositoryTest: BaseITTest() {
+
+    @Test
+    fun name() {
+        // given
+
+        // when
+
+
+        // then
+    }
+}
